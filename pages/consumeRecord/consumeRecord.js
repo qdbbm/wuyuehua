a021720a05d29e9142df5c2bc2302addec05f414
zzw118 <2238662003@qq.com>
2019-03-04 15:28:57 +0800

    消费成功、消费记录

---
+++ pages/consumeRecord/consumeRecord.js
@@ -0,0 +1,102 @@
+// pages/consumeRecord/consumeRecord.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    recordList: [
+      {
+        tle: '五月花酒楼哈西门店消费',
+        time: '2018-01-20  12:30:20',
+        total: '200',
+        reduce: 20,
+        pay: 180
+      },
+      {
+        tle: '五月花酒楼哈西门店消费',
+        time: '2018-01-20  12:30:20',
+        total: '200',
+        reduce: 20,
+        pay: 180
+      },
+      {
+        tle: '五月花酒楼哈西门店消费',
+        time: '2018-01-20  12:30:20',
+        total: '200',
+        reduce: 20,
+        pay: 180
+      },
+      {
+        tle: '五月花酒楼哈西门店消费',
+        time: '2018-01-20  12:30:20',
+        total: '200',
+        reduce: 20,
+        pay: 180
+      },
+      {
+        tle: '五月花酒楼哈西门店消费',
+        time: '2018-01-20  12:30:20',
+        total: '200',
+        reduce: 20,
+        pay: 180
+      }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
